5711c6848d740c01d8f7df79bf9de2d3557980d6
imrancu <mdimran3772@gmail.com>
2022-03-16 21:27:50 +0600

    updated

--- js/countries.js
+++ js/countries.js
@@ -4,17 +4,43 @@ const loadCounries = () => {
         .then(data => displayCountries(data));
 };
 
+
 const displayCountries = countries => {
     const countriesHTML = countries.map(country => getCountryHTML(country));
     // console.log(countriesHTML[0]);
     const container = document.getElementById('countries');
-    container.innerHTML = countriesHTML;
+    container.innerHTML = countriesHTML.join(' ');
 };
 
-const getCountryHTML = country => {
+// Original
+// const getCountryHTML = country => {
+//     return `
+//             <div class="country">
+//               <h2>${country.name.common}</h2>
+//               <img src="${country.flags.png}">
+//             </div>
+//           `;
+// }
+
+// option 1
+// const getCountryHTML = country => {
+//   const {name, flags} = country
+//     return `
+//             <div class="country">
+//               <h2>${name.common}</h2>
+//               <img src="${flags.png}">
+//             </div>
+//           `;
+// }
+
+// Option 2
+const getCountryHTML = ({name, flags, area, region}) => {
     return `
-            <div>
-              <h2>${country.name.common}</h2>
+            <div class="country">
+              <h2>${name.common}</h2>
+              <p>Area: ${area}</p>
+              <p>Continent: ${region}</p>
+              <img src="${flags.png}">
             </div>
           `;
 }
